d8feae779f8e69249ef648ab9db1abf3ad302008
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-08-27 17:25:00 -0400

    cleaned native test

--- contracts/donations/src/test.rs
+++ contracts/donations/src/test.rs
@@ -3,12 +3,12 @@
 extern crate std;
 // use soroban_sdk::testutils::{register_test_contract as register_donation, Donation};
 use soroban_sdk::{
-    testutils::{Address as AddressTestTrait, Events, Ledger},
+    testutils::{Address as AddressTestTrait},
 
-    token, vec, Address, Env, IntoVal, Symbol, Val, Vec, xdr::Asset::Native as native, Bytes, BytesN, String
+    token, Address, Env, Bytes
 };
 
-use soroban_sdk::xdr::{Asset, Hash, HashIdPreimage, ContractIdPreimage, WriteXdr, HashIdPreimageContractId};
+use soroban_sdk::xdr::{Asset, ContractIdPreimage, WriteXdr};
 
 use crate::{DonationsClient};
 
@@ -23,7 +23,6 @@ fn create_donations_contract<'a>(
 }
 
 fn native_asset_contract_address(e: &Env) -> Address {
-    let env = Env::default();
     let native_asset = Asset::Native;
     let contract_id_preimage = ContractIdPreimage::Asset(native_asset);
     let bytes = Bytes::from_slice(&e, &contract_id_preimage.to_xdr().unwrap());
@@ -32,20 +31,20 @@ fn native_asset_contract_address(e: &Env) -> Address {
 }
 
 #[test]
-fn test() {
+fn test_native() {
     let e: Env = Default::default();
     e.mock_all_auths();
 
     // Set the identities that will be used in the test
-    let creator = Address::random(&e);
+    let _creator = Address::random(&e);
     let recipient = Address::random(&e);
-    let donor = Address::random(&e);
+    let _donor = Address::random(&e);
     
     // Set the native token address
-    let native_address =native_asset_contract_address(&e);
-    let expected_address_string="CDF3YSDVBXV3QU2QSOZ55L4IVR7UZ74HIJKXNJMN4K5MOVFM3NDBNMLY";
     // TODO: Test the address. Not not working
     // Check this: https://discord.com/channels/897514728459468821/1145458616086843423/1145458616086843423
+    let native_address =native_asset_contract_address(&e);
+    //let expected_address_string="CDF3YSDVBXV3QU2QSOZ55L4IVR7UZ74HIJKXNJMN4K5MOVFM3NDBNMLY";
     //assert_eq!(native_address, expected_address_string);
     
 
@@ -56,12 +55,13 @@ fn test() {
     assert_eq!(donations.recipient(), recipient);
     assert_eq!(donations.token(), native_address);
 
-
-    // Create a native token client
-    let native_client = token::Client::new(&e, &native_address);
-    // Call some function in the native token
-    // TODO: We cannot call functions in the native token:
-    // Check this: https://discord.com/channels/897514728459468821/1145462925109231726/1145462925109231726
+    /*
+    Create a native token client
+    Call some function in the native token
+    TODO: We cannot call functions in the native token:
+    Check this: https://discord.com/channels/897514728459468821/1145462925109231726/1145462925109231726
+    */
+    //let native_client = token::Client::new(&e, &native_address);
     // assert_eq!(native_client.name(), String::from_slice(&e, "native"));
 
 }
