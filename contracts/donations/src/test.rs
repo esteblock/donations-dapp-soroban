ca6b5b15b2a92fa61a335d05ad60d190b8cb936f
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-08-27 17:40:52 -0400

    test non_native token OK

--- contracts/donations/src/test.rs
+++ contracts/donations/src/test.rs
@@ -2,13 +2,21 @@
 
 extern crate std;
 // use soroban_sdk::testutils::{register_test_contract as register_donation, Donation};
-use soroban_sdk::{
-    testutils::{Address as AddressTestTrait},
-
+use soroban_sdk::{ testutils::{Address as AddressTestTrait},
     token, Address, Env, Bytes
 };
-
 use soroban_sdk::xdr::{Asset, ContractIdPreimage, WriteXdr};
+use token::AdminClient as TokenAdminClient;
+use token::Client as TokenClient;
+
+fn create_token_contract<'a>(e: &Env, admin: &Address) -> (TokenClient<'a>, TokenAdminClient<'a>) {
+    let contract_address = e.register_stellar_asset_contract(admin.clone());
+    (
+        TokenClient::new(e, &contract_address),
+        TokenAdminClient::new(e, &contract_address),
+    )
+}
+
 
 use crate::{DonationsClient};
 
@@ -30,13 +38,16 @@ fn native_asset_contract_address(e: &Env) -> Address {
     native_asset_address
 }
 
+// fn create_token_contract<'a>(e: &Env, admin: &Address) -> token::Client<'a> {
+//     token::Client::new(e, &e.register_stellar_asset_contract(admin.clone()))
+// }
+
 #[test]
 fn test_native() {
     let e: Env = Default::default();
     e.mock_all_auths();
 
     // Set the identities that will be used in the test
-    let _creator = Address::random(&e);
     let recipient = Address::random(&e);
     let _donor = Address::random(&e);
     
@@ -63,5 +74,52 @@ fn test_native() {
     */
     //let native_client = token::Client::new(&e, &native_address);
     // assert_eq!(native_client.name(), String::from_slice(&e, "native"));
+
+}
+
+#[test]
+fn test_non_native() {
+    let e: Env = Default::default();
+    e.mock_all_auths();
+
+    // Set the identities that will be used in the test
+    let recipient = Address::random(&e);
+    let donor = Address::random(&e);
+
+    // Let's create a random token
+    let admin = Address::random(&e);
+    let (token, token_admin) = create_token_contract(&e, &admin);
+    //let token = create_token_contract(&e, &admin);
+    
+    // Deploy the donations contract with the recipient and the token address
+    let donations = create_donations_contract(&e, &recipient, &token.address);
+
+    // Let's test basic configuration:
+    assert_eq!(donations.recipient(), recipient);
+    assert_eq!(donations.token(), token.address);
+
+    // Initial balances
+    token_admin.mint(&donor, &1000);
+    assert_eq!(token.balance(&donor), 1000);
+    assert_eq!(token.balance(&recipient), 0);
+    assert_eq!(token.balance(&donations.address), 0);
+
+    // First donation on 10 units
+    donations.donate(&donor, &10);
+    assert_eq!(token.balance(&donor), 990);
+    assert_eq!(token.balance(&recipient), 0);
+    assert_eq!(token.balance(&donations.address), 10);
+
+    // First donation on 10 units
+    donations.donate(&donor, &10);
+    assert_eq!(token.balance(&donor), 980);
+    assert_eq!(token.balance(&recipient), 0);
+    assert_eq!(token.balance(&donations.address), 20);
+
+    // Withdraw
+    donations.withdraw();
+    assert_eq!(token.balance(&donor), 980);
+    assert_eq!(token.balance(&recipient), 20);
+    assert_eq!(token.balance(&donations.address), 0);
 
 }
